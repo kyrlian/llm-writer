0dbb071790ac98ed2efcba04d297c9eebde8cded
Rémi Serra <remi.serra@fr.ibm.com>
2024-05-09 18:43:08 +0200

    improve prompt

--- app/prompts.py
+++ app/prompts.py
@@ -1,4 +1,4 @@
 
 prompt_summarize = "Please summarize the following TEXT, do no add comments:\n\nTEXT\n\n{fulltext}\n"
 
-prompt_suggest = "Please read the STORY SO FAR below, and the provided INSTRUCTIONS, then please complete the story following the provided instructions.\n\nSTORY SO FAR\n\n{fullsummary}\n\nINSTRUCTIONS\n\n{instructions}\n"
+prompt_suggest = "Please read the STORY SO FAR below, and the provided INSTRUCTIONS, then please complete the story following the provided instructions. Do not add comments.\n\nSTORY SO FAR\n\n{fullsummary}\n\nINSTRUCTIONS\n\n{instructions}\n"
