069c0c1c115c6ed918b7de5f87f3cb4986e4d07c
kyrlian <kyrlian@gmail.com>
2024-08-04 19:32:46 +0200

    move file and add style

---
+++ src/app.py
@@ -0,0 +1,143 @@
+import gradio as gr
+
+from ollama import generate_response
+from prompts import prompts
+from fileio import load, save
+from src import style
+from style import Style
+
+sdxlturbo_loaded = False
+try:
+    from sdxlturbo import sdxlturboPipeline
+
+    sdxlturbo_loaded = True
+except ImportError:
+    print(f"Couldn't load sdxlturbo, deactivating image generation")
+
+# Functions
+def changelang(lang):
+    return prompts[lang]["prompt_suggest"], prompts[lang]["prompt_summarize"]
+
+
+def suggest(suggestpprompt, fulltext, fullsummary, instructions):
+    prompt = suggestpprompt.format(
+        fulltext=fulltext, fullsummary=fullsummary, instructions=instructions
+    )
+    return generate_response(prompt)
+
+
+def summarize(summaryprompt, fulltext, tmptext):
+    prompt = summaryprompt.format(fulltext=fulltext, tmptext=tmptext)
+    return generate_response(prompt)
+
+
+def generate_image(imglist, prompt):
+    if sdxlturbo_loaded:
+        pipe = sdxlturboPipeline()
+        img = pipe.generate(prompt)
+        if imglist is None:
+            imglist = []
+        imglist.append(img)
+    return imglist
+
+
+def accept(fulltext, tmptext, fullsummary, tmpsummary, imglist):
+    return (
+        f"{fulltext}\n\n{tmptext}",
+        f"{fullsummary}\n\n{tmpsummary}",
+        generate_image(imglist, tmpsummary),
+    )
+
+# Init
+initialtext, initialsummary = load()
+default_lang = list(prompts.keys())[0]
+prompt_suggest, prompt_summarize = changelang(default_lang)
+
+# App
+# https://www.gradio.app/guides/blocks-and-event-listeners#blocks-structure
+with gr.Blocks(theme=style.Style()) as demo:
+    gr.Markdown("# LLM Writer")
+    # https://www.gradio.app/guides/controlling-layout#rows
+    with gr.Accordion(label="Setup",open=True):
+        lang_drop = gr.Dropdown(
+            choices=list(prompts.keys()),
+            value=default_lang,
+            label="Language",
+            show_label=False,
+        )
+        with gr.Row():
+            suggestpprompt_box = gr.Textbox(
+                value=prompt_suggest,
+                label="Suggestion prompt - use {fulltext}, {fullsummary}, {instructions} placeholders",
+            )
+            summaryprompt_box = gr.Textbox(
+                value=prompt_summarize,
+                label="Summary prompt - use {fulltext}, {tmptext} placeholders",
+            )
+        model_drop = gr.Dropdown(label="Model")# TODO get ollama models
+        if sdxlturbo_loaded:
+            generate_image_check = gr.Checkbox(label="Generate Image with sdxlturbo",value=True)
+    fulltext_box = gr.Textbox(
+        value=initialtext, label="Full Text - {fulltext}"
+    )
+    fullsummary_box = gr.Textbox(
+        value=initialsummary, label="Full Summary - {fullsummary}"
+    )
+
+    instructions_box = gr.Textbox(label="Instructions - {instructions}")
+    suggest_btn = gr.Button("Suggest")
+    tmptext_box = gr.Textbox(label="Temp text - {tmptext}")
+    summarize_btn = gr.Button("Summarize")
+    tmpsummary_box = gr.Textbox(label="Temp summary")
+    accept_btn = gr.Button("Accept")
+    # add option to generate image
+    save_btn = gr.Button("Save to file")
+    status_box = gr.Markdown(label="Status")
+    img_gallery = gr.Gallery(label="Illustrations",rows=1, columns=3, interactive=False, visible=sdxlturbo_loaded)
+
+    # match button - function
+    lang_drop.change(
+        fn=changelang,
+        inputs=[lang_drop],
+        outputs=[suggestpprompt_box, summaryprompt_box],
+        api_name="changelang",
+    )
+    suggest_btn.click(
+        fn=suggest,
+        inputs=[
+            suggestpprompt_box,
+            fulltext_box,
+            fullsummary_box,
+            instructions_box,
+        ],
+        outputs=tmptext_box,
+        api_name="suggest",
+    )
+    summarize_btn.click(
+        fn=summarize,
+        inputs=[summaryprompt_box, fulltext_box, tmptext_box],
+        outputs=tmpsummary_box,
+        api_name="summarize",
+    )
+    accept_btn.click(
+        fn=accept,
+        inputs=[
+            fulltext_box,
+            tmptext_box,
+            fullsummary_box,
+            tmpsummary_box,
+            img_gallery,
+        ],
+        outputs=[fulltext_box, fullsummary_box, img_gallery],
+        api_name="accept",
+    )
+    save_btn.click(
+        fn=save,
+        inputs=[fulltext_box, fullsummary_box],
+        outputs=status_box,
+        api_name="save",
+    )
+
+
+if __name__ == "__main__":
+    demo.launch()
--- src/prompts.py
+++ src/prompts.py
@@ -1,9 +1,9 @@
 prompts = {
-    "en": {
+    "English": {
         "prompt_summarize": "Please summarize the following TEXT in one or two sentences, do no add comments:\n\nTEXT\n\n{tmptext}\n",
         "prompt_suggest": "Please read the STORY SO FAR below, and the provided INSTRUCTIONS, then please complete the story following the provided instructions. Do not add comments, only print the summary.\n\nSTORY SO FAR\n\n{fullsummary}\n\nINSTRUCTIONS\n\n{instructions}\n",
     },
-    "fr": {
+    "French": {
         "prompt_summarize": "Veuillez résumer le TEXTE suivant en une ou deux phrases, sans ajouter de commentaires :\n\nTEXTE\n\n{tmptext}\n",
         "prompt_suggest": "Veuillez lire l'HISTOIRE JUSQUE ICI ci-dessous et les INSTRUCTIONS fournies, puis complétez l'histoire en suivant les instructions fournies. N'ajoutez pas de commentaires, affichez seulement le résumé.\n\nHISTOIRE JUSQUE ICI\n\n{fullsummary}\n\nINSTRUCTIONS\n\n{instructions}\n",
     },
