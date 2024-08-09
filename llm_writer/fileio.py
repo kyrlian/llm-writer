9d35127a44dba8b84098f6ffd96572b1bac7b5e8
kyrlian <kyrlian@gmail.com>
2024-08-09 12:50:06 +0200

    named file

--- llm_writer/fileio.py
+++ llm_writer/fileio.py
@@ -1,8 +1,9 @@
+import os
 from datetime import datetime
 
-inputdir = "inputs"
-outputdir = "outputs"
-textfile = "text.txt"
+default_inputdir = "inputs"
+default_outputdir = "outputs"
+default_textfile = "text.txt"
 # summaryfile = "summary.txt"
 
 def read(filename):
@@ -17,13 +18,19 @@ def write(filename, s):
     f.close()
 
 
-def load():
-    initialtext = read(inputdir + "/" + textfile)
+def load(filename=None):
+    if filename is None:
+        filename = default_inputdir + "/" + default_textfile
+    initialtext = read(filename)
     return initialtext
 
 
-def save(fulltext):
+def save(fulltext, filename=None, add_timestamp=True):
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    write(f"{outputdir}/{timestamp}-{textfile}", fulltext)
-    # write(f"{outputdir}/{timestamp}-{summaryfile}", fullsummary)
-    return f"Saved to {outputdir}"
+    if filename is None:
+        filename=f"{default_outputdir}/{timestamp}-{default_textfile}"
+    elif add_timestamp:
+        f_name, f_ext = os.path.splitext(filename)
+        filename = f'{f_name}-{timestamp}{f_ext}'
+    write(filename, fulltext)
+    return f"Saved to {filename}"
