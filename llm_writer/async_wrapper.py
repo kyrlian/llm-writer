5cbc569746dbf3582512a9a177ee01a8b029e6d3
kyrlian <kyrlian@gmail.com>
2024-08-11 12:45:03 +0200

    simpler async_wrapper

--- llm_writer/async_wrapper.py
+++ llm_writer/async_wrapper.py
@@ -2,17 +2,25 @@
 import asyncio
 from typing import AsyncGenerator, Generator
 
-async def async_wrapper(normal_generator:Generator)-> AsyncGenerator:
-    """ Convert a normal generator to an async generator """
-    async def getnext(normal_generator):
-        try:
-            v = next(normal_generator)
-            return v,True
-        except StopIteration as e:
-            return None, False
+# async def async_wrapper(normal_generator:Generator)-> AsyncGenerator:
+#     """ Convert a normal generator to an async generator """
+#     async def getnext(normal_generator):
+#         try:
+#             v = next(normal_generator)
+#             return v,True
+#         except StopIteration as e:
+#             return None, False
         
-    doloop = True
-    while doloop:
-        val, doloop = await asyncio.create_task(getnext(normal_generator))
-        if doloop:
-            yield val
+#     doloop = True
+#     while doloop:
+#         val, doloop = await asyncio.create_task(getnext(normal_generator))
+#         if doloop:
+#             yield val
+
+
+async def async_wrapper(normal_generator:Generator)-> AsyncGenerator:
+    async def g(a):
+        return a
+
+    for v in normal_generator:
+        yield await asyncio.create_task(g(v))
