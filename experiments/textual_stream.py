5515abcb1e2cfc5b0101ffc4dca8a3d5570ae15c
kyrlian <kyrlian@gmail.com>
2024-08-11 12:44:44 +0200

    simpler async_wrapper

--- experiments/textual_stream.py
+++ experiments/textual_stream.py
@@ -15,21 +15,28 @@ def demo_generator(n: int)-> Generator:
     yield "done"
 
 
+# async def async_wrapper_old(normal_generator:Generator)-> AsyncGenerator:
+#     """ Convert a normal generator to an async generator """
+#     async def getnext(normal_generator):
+#         try:
+#             v = next(normal_generator)
+#             return v,True
+#         except StopIteration as e:
+#             return None, False
+        
+#     doloop = True
+#     while doloop:
+#         val, doloop = await asyncio.create_task(getnext(normal_generator))
+#         if doloop:
+#             yield val
+        
 async def async_wrapper(normal_generator:Generator)-> AsyncGenerator:
-    """ Convert a normal generator to an async generator """
-    async def getnext(normal_generator):
-        try:
-            v = next(normal_generator)
-            return v,True
-        except StopIteration as e:
-            return None, False
-        
-    doloop = True
-    while doloop:
-        val, doloop = await asyncio.create_task(getnext(normal_generator))
-        if doloop:
-            yield val
-        
+    async def g(a):
+        return a
+
+    for v in normal_generator:
+        yield await asyncio.create_task(g(v))
+
 class StreamedTextArea(TextArea):
     """A subclass of TextArea to call parse_and_generate_stream on, 'enter'."""
     # WORKS
