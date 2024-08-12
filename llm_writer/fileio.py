ec6ebed576e309d39700f30ecc2f78f60d858573
kyrlian <kyrlian@gmail.com>
2024-08-12 09:35:44 +0200

    simplify

--- llm_writer/fileio.py
+++ llm_writer/fileio.py
@@ -6,25 +6,15 @@ default_outputdir = "outputs"
 default_textfile = "text.txt"
 # summaryfile = "summary.txt"
 
-def read(filename):
+def load(filename=None):
+    if filename is None:
+        filename = default_inputdir + "/" + default_textfile
     try:
         f = open(filename, "r")
         lines = f.readlines()
         return "\n".join(lines)
     except FileNotFoundError:
         return ""
-
-def write(filename, s):
-    f = open(filename, "w")
-    f.write(s)
-    f.close()
-
-
-def load(filename=None):
-    if filename is None:
-        filename = default_inputdir + "/" + default_textfile
-    initialtext = read(filename)
-    return initialtext
 
 
 def save(fulltext, filename=None, add_timestamp=True):
@@ -34,5 +24,7 @@ def save(fulltext, filename=None, add_timestamp=True):
     elif add_timestamp:
         f_name, f_ext = os.path.splitext(filename)
         filename = f'{f_name}-{timestamp}{f_ext}'
-    write(filename, fulltext)
+    with open(filename, "w") as f:
+        f.write(fulltext)
+        f.close()
     return f"Saved to {filename}"
